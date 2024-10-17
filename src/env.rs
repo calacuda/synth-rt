57eb9db4aa36ec2be1aa64791e8f5291ec267b88
Eoghan West <eowest@gmail.com>
2024-10-17 05:03:00 -0400

    back up

--- src/env.rs
+++ src/env.rs
@@ -17,7 +17,7 @@ pub struct ADSR {
 
 impl ADSR {
     pub fn new() -> Self {
-        let base_params = [0.0, 0.001, -0.001, 0.5, 0.01];
+        let base_params = [0.0, 0.001, -0.001, 0.5, 0.5];
 
         Self {
             sample_rate: SAMPLE_RATE,
