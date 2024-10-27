2224c5fd3ad2b2a50d83d8b792257df2c60d6533
Eoghan West <eowest@gmail.com>
2024-10-27 10:16:21 -0400

    test2 the electric boogaloo

--- src/bin/synth-rt.rs
+++ src/bin/synth-rt.rs
@@ -4,7 +4,9 @@ use bevy::{
     prelude::*,
     sprite::Anchor,
     text::{BreakLineOn, Text2dBounds},
+    winit::WinitSettings,
 };
+use bevy_framepace::{FramepaceSettings, Limiter};
 use midi_control::{ControlEvent, KeyEvent, MidiMessage};
 use rodio::OutputStream;
 use serialport;
@@ -41,186 +43,297 @@ fn main() {
 
     // run_midi(synth)
     App::new()
+        .insert_resource(SynthMarker(synth))
         .add_plugins(DefaultPlugins)
+        .insert_resource(WinitSettings::desktop_app())
+        .add_plugins(bevy_framepace::FramepacePlugin)
+        .add_systems(Startup, set_framerate)
         .add_systems(Startup, setup)
-        .add_systems(
-            Update,
-            (animate_translation, animate_rotation, animate_scale),
-        )
+        // .add_systems(
+        //     Update,
+        //     (animate_translation, animate_rotation, animate_scale),
+        // )
         .run();
 }
 
-#[derive(Component)]
-struct AnimateTranslation;
-
-#[derive(Component)]
-struct AnimateRotation;
-
-#[derive(Component)]
-struct AnimateScale;
+fn set_framerate(mut frame_settings: ResMut<FramepaceSettings>) {
+    frame_settings.limiter = Limiter::from_framerate(20.0);
+}
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
-    let text_style = TextStyle {
-        font: font.clone(),
-        font_size: 60.0,
-        ..default()
-    };
-    let text_justification = JustifyText::Center;
-    // 2d camera
-    commands.spawn(Camera2dBundle::default());
-    // Demonstrate changing translation
-    commands.spawn((
-        Text2dBundle {
-            text: Text::from_section("translation", text_style.clone())
-                .with_justify(text_justification),
-            ..default()
-        },
-        AnimateTranslation,
-    ));
-    // Demonstrate changing rotation
-    commands.spawn((
-        Text2dBundle {
-            text: Text::from_section("rotation", text_style.clone())
-                .with_justify(text_justification),
-            ..default()
-        },
-        AnimateRotation,
-    ));
-    // Demonstrate changing scale
-    commands.spawn((
-        Text2dBundle {
-            text: Text::from_section("scale", text_style).with_justify(text_justification),
-            transform: Transform::from_translation(Vec3::new(400.0, 0.0, 0.0)),
-            ..default()
-        },
-        AnimateScale,
-    ));
-    // Demonstrate text wrapping
-    let slightly_smaller_text_style = TextStyle {
-        font,
-        font_size: 42.0,
-        ..default()
-    };
-    let box_size = Vec2::new(300.0, 200.0);
-    let box_position = Vec2::new(0.0, -250.0);
+    // Camera
+    commands.spawn((Camera2dBundle::default(), IsDefaultUiCamera));
+
+    // root node
     commands
-        .spawn(SpriteBundle {
-            sprite: Sprite {
-                color: Color::srgb(0.25, 0.25, 0.75),
-                custom_size: Some(Vec2::new(box_size.x, box_size.y)),
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                justify_content: JustifyContent::SpaceBetween,
                 ..default()
             },
-            transform: Transform::from_translation(box_position.extend(0.0)),
             ..default()
         })
-        .with_children(|builder| {
-            builder.spawn(Text2dBundle {
-                text: Text {
-                    sections: vec![TextSection::new(
-                        "this text wraps in the box\n(Unicode linebreaks)",
-                        slightly_smaller_text_style.clone(),
-                    )],
-                    justify: JustifyText::Left,
-                    linebreak_behavior: BreakLineOn::WordBoundary,
-                },
-                text_2d_bounds: Text2dBounds {
-                    // Wrap text in the rectangle
-                    size: box_size,
-                },
-                // ensure the text is drawn on top of the box
-                transform: Transform::from_translation(Vec3::Z),
-                ..default()
-            });
-        });
-
-    let other_box_size = Vec2::new(300.0, 200.0);
-    let other_box_position = Vec2::new(320.0, -250.0);
-    commands
-        .spawn(SpriteBundle {
-            sprite: Sprite {
-                color: Color::srgb(0.20, 0.3, 0.70),
-                custom_size: Some(Vec2::new(other_box_size.x, other_box_size.y)),
-                ..default()
-            },
-            transform: Transform::from_translation(other_box_position.extend(0.0)),
-            ..default()
-        })
-        .with_children(|builder| {
-            builder.spawn(Text2dBundle {
-                text: Text {
-                    sections: vec![TextSection::new(
-                        "this text wraps in the box\n(AnyCharacter linebreaks)",
-                        slightly_smaller_text_style.clone(),
-                    )],
-                    justify: JustifyText::Left,
-                    linebreak_behavior: BreakLineOn::AnyCharacter,
-                },
-                text_2d_bounds: Text2dBounds {
-                    // Wrap text in the rectangle
-                    size: other_box_size,
-                },
-                // ensure the text is drawn on top of the box
-                transform: Transform::from_translation(Vec3::Z),
-                ..default()
-            });
-        });
-
-    for (text_anchor, color) in [
-        (Anchor::TopLeft, Color::Srgba(RED)),
-        (Anchor::TopRight, Color::Srgba(LIME)),
-        (Anchor::BottomRight, Color::Srgba(BLUE)),
-        (Anchor::BottomLeft, Color::Srgba(YELLOW)),
-    ] {
-        commands.spawn(Text2dBundle {
-            text: Text {
-                sections: vec![TextSection::new(
-                    format!(" Anchor::{text_anchor:?} "),
-                    TextStyle {
-                        color,
-                        ..slightly_smaller_text_style.clone()
+        .with_children(|parent| {
+            // left vertical fill (border)
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Px(200.),
+                        border: UiRect::all(Val::Px(2.)),
+                        ..default()
                     },
-                )],
-                ..Default::default()
-            },
-            transform: Transform::from_translation(250. * Vec3::Y),
-            text_anchor,
-            ..default()
+                    background_color: Color::srgb(0.65, 0.65, 0.65).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // left vertical fill (content)
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                width: Val::Percent(100.),
+                                flex_direction: FlexDirection::Column,
+                                padding: UiRect::all(Val::Px(5.)),
+                                row_gap: Val::Px(5.),
+                                ..default()
+                            },
+                            background_color: Color::srgb(0.15, 0.15, 0.15).into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            // text
+                            parent.spawn((
+                                TextBundle::from_section(
+                                    "Text Example",
+                                    TextStyle {
+                                        font: asset_server.load("fonts/Anonymous Pro B.ttf"),
+                                        font_size: 30.0,
+                                        ..default()
+                                    },
+                                ),
+                                // Because this is a distinct label widget and
+                                // not button/list item text, this is necessary
+                                // for accessibility to treat the text accordingly.
+                                Label,
+                            ));
+
+                            #[cfg(feature = "bevy_dev_tools")]
+                            // Debug overlay text
+                            parent.spawn((
+                                TextBundle::from_section(
+                                    "Press Space to enable debug outlines.",
+                                    TextStyle {
+                                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                        ..default()
+                                    },
+                                ),
+                                Label,
+                            ));
+
+                            #[cfg(not(feature = "bevy_dev_tools"))]
+                            parent.spawn((
+                                TextBundle::from_section(
+                                    "Try enabling feature \"bevy_dev_tools\".",
+                                    TextStyle {
+                                        font: asset_server.load("fonts/Anonymous Pro B.ttf"),
+                                        ..default()
+                                    },
+                                ),
+                                Label,
+                            ));
+                        });
+                });
+            // right vertical fill
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Column,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        width: Val::Px(200.),
+                        ..default()
+                    },
+                    background_color: Color::srgb(0.15, 0.15, 0.15).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // Title
+                    parent.spawn((
+                        TextBundle::from_section(
+                            "Scrolling list",
+                            TextStyle {
+                                font: asset_server.load("fonts/Anonymous Pro B.ttf"),
+                                font_size: 25.,
+                                ..default()
+                            },
+                        ),
+                        Label,
+                    ));
+                    // List with hidden overflow
+                    parent.spawn(NodeBundle {
+                        style: Style {
+                            flex_direction: FlexDirection::Column,
+                            align_self: AlignSelf::Stretch,
+                            height: Val::Percent(50.),
+                            overflow: Overflow::clip_y(),
+                            ..default()
+                        },
+                        background_color: Color::srgb(0.10, 0.10, 0.10).into(),
+                        ..default()
+                    });
+                });
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Px(200.0),
+                        height: Val::Px(200.0),
+                        position_type: PositionType::Absolute,
+                        left: Val::Px(210.),
+                        bottom: Val::Px(10.),
+                        border: UiRect::all(Val::Px(20.)),
+                        ..default()
+                    },
+                    border_color: LIME.into(),
+                    background_color: Color::srgb(0.4, 0.4, 1.).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(NodeBundle {
+                        style: Style {
+                            width: Val::Percent(100.0),
+                            height: Val::Percent(100.0),
+                            ..default()
+                        },
+                        background_color: Color::srgb(0.8, 0.8, 1.).into(),
+                        ..default()
+                    });
+                });
+            // render order test: reddest in the back, whitest in the front (flex center)
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        position_type: PositionType::Absolute,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                width: Val::Px(100.0),
+                                height: Val::Px(100.0),
+                                ..default()
+                            },
+                            background_color: Color::srgb(1.0, 0.0, 0.).into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            parent.spawn(NodeBundle {
+                                style: Style {
+                                    // Take the size of the parent node.
+                                    width: Val::Percent(100.0),
+                                    height: Val::Percent(100.0),
+                                    position_type: PositionType::Absolute,
+                                    left: Val::Px(20.),
+                                    bottom: Val::Px(20.),
+                                    ..default()
+                                },
+                                background_color: Color::srgb(1.0, 0.3, 0.3).into(),
+                                ..default()
+                            });
+                            parent.spawn(NodeBundle {
+                                style: Style {
+                                    width: Val::Percent(100.0),
+                                    height: Val::Percent(100.0),
+                                    position_type: PositionType::Absolute,
+                                    left: Val::Px(40.),
+                                    bottom: Val::Px(40.),
+                                    ..default()
+                                },
+                                background_color: Color::srgb(1.0, 0.5, 0.5).into(),
+                                ..default()
+                            });
+                            parent.spawn(NodeBundle {
+                                style: Style {
+                                    width: Val::Percent(100.0),
+                                    height: Val::Percent(100.0),
+                                    position_type: PositionType::Absolute,
+                                    left: Val::Px(60.),
+                                    bottom: Val::Px(60.),
+                                    ..default()
+                                },
+                                background_color: Color::srgb(1.0, 0.7, 0.7).into(),
+                                ..default()
+                            });
+                            // alpha test
+                            parent.spawn(NodeBundle {
+                                style: Style {
+                                    width: Val::Percent(100.0),
+                                    height: Val::Percent(100.0),
+                                    position_type: PositionType::Absolute,
+                                    left: Val::Px(80.),
+                                    bottom: Val::Px(80.),
+                                    ..default()
+                                },
+                                background_color: Color::srgba(1.0, 0.9, 0.9, 0.4).into(),
+                                ..default()
+                            });
+                        });
+                });
+            // bevy logo (flex center)
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        position_type: PositionType::Absolute,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::FlexStart,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // bevy logo (image)
+                    // A `NodeBundle` is used to display the logo the image as an `ImageBundle` can't automatically
+                    // size itself with a child node present.
+                    parent
+                        .spawn((
+                            NodeBundle {
+                                style: Style {
+                                    width: Val::Px(500.0),
+                                    height: Val::Px(125.0),
+                                    margin: UiRect::top(Val::VMin(5.)),
+                                    ..default()
+                                },
+                                ..default()
+                            },
+                            UiImage::new(asset_server.load("branding/bevy_logo_dark_big.png")),
+                        ))
+                        .with_children(|parent| {
+                            // alt text
+                            // This UI node takes up no space in the layout and the `Text` component is used by the accessibility module
+                            // and is not rendered.
+                            parent.spawn((
+                                NodeBundle {
+                                    style: Style {
+                                        display: Display::None,
+                                        ..Default::default()
+                                    },
+                                    ..Default::default()
+                                },
+                                Text::from_section("Bevy logo", TextStyle::default()),
+                            ));
+                        });
+                });
         });
-    }
 }
 
-fn animate_translation(
-    time: Res<Time>,
-    mut query: Query<&mut Transform, (With<Text>, With<AnimateTranslation>)>,
-) {
-    for mut transform in &mut query {
-        transform.translation.x = 100.0 * time.elapsed_seconds().sin() - 400.0;
-        transform.translation.y = 100.0 * time.elapsed_seconds().cos();
-    }
-}
-
-fn animate_rotation(
-    time: Res<Time>,
-    mut query: Query<&mut Transform, (With<Text>, With<AnimateRotation>)>,
-) {
-    for mut transform in &mut query {
-        transform.rotation = Quat::from_rotation_z(time.elapsed_seconds().cos());
-    }
-}
-
-fn animate_scale(
-    time: Res<Time>,
-    mut query: Query<&mut Transform, (With<Text>, With<AnimateScale>)>,
-) {
-    // Consider changing font-size instead of scaling the transform. Scaling a Text2D will scale the
-    // rendered quad, resulting in a pixellated look.
-    for mut transform in &mut query {
-        let scale = (time.elapsed_seconds().sin() + 1.1) * 2.0;
-        transform.scale.x = scale;
-        transform.scale.y = scale;
-    }
-}
 // pub fn decode_hex(s: &str) -> Result<Vec<u8>> {
 //     let s = s.replace("\r\n", "");
 //     // println!("{s:#?}");
